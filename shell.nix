19139e470274c26b8e57d26002844d1a2178d0da
mtwtkman <punipuniomochi@gmail.com>
2022-03-30 09:42:54 +0900

    install fp-ts

--- shell.nix
+++ shell.nix
@@ -1,14 +1,6 @@
 with import <nixpkgs> {};
 mkShell {
     packages = [
-        elmPackages.elm
-        elmPackages.elm-format
-        elmPackages.elm-test
-        ghc
-        python310
         nodejs
     ];
-    shellHook = ''
-    npm install uglify-js
-    '';
 }
