b501b38e9008c2687787cfb4d7833b05ac4bf3f3
mtwtkman <punipuniomochi@gmail.com>
2022-03-31 18:26:40 +0900

    rename jsfile

--- Bundle.hs
+++ Bundle.hs
@@ -8,7 +8,7 @@ styleTag :: String -> String
 styleTag src = "<link rel=\"stylesheet\" href=\"" ++ src ++ "\">"
 
 buildHtml :: String -> String -> String
-buildHtml appjsdef initjsdef = concat [
+buildHtml appjsdef porsjsdef = concat [
     "<html>"
     , "<head>"
     , "<meta charset=\"utf8\">"
@@ -17,7 +17,7 @@ buildHtml appjsdef initjsdef = concat [
     , appjsdef
     , "</script></head>"
     , "<body><main></main><script>"
-    , initjsdef
+    , porsjsdef
     , "</script></body>"
     ,"</html>"
     ]
@@ -26,9 +26,9 @@ readAppJavascript :: IO String
 readAppJavascript =
     readFile "dist/app.js"
 
-readInitJavascript :: IO String
-readInitJavascript =
-    readFile "dist/init.js"
+readPortsJavascript :: IO String
+readPortsJavascript =
+    readFile "dist/ports.js"
 
 minifyStyleSheet :: String -> String
 minifyStyleSheet = filter $ not . isSpace
@@ -50,13 +50,13 @@ indexHtml :: FilePath
 indexHtml = appDirectory ++ "/index.html"
 
 build :: String -> String -> IO ()
-build appjsdef initjsdef = do
-    writeFile indexHtml (buildHtml appjsdef initjsdef)
+build appjsdef porsjsdef = do
+    writeFile indexHtml (buildHtml appjsdef porsjsdef)
 
 main :: IO ()
 main = do
     setup
     appjsdef <- readAppJavascript
-    initjsdef <- readInitJavascript
-    build appjsdef initjsdef
+    porsjsdef <- readPortsJavascript
+    build appjsdef porsjsdef
     putStr $ "Generated " ++ indexHtml
